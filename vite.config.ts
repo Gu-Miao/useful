e2fdf5fc7a9726b0f7b29a74e8f8ba87c434223c
Gu-Miao <1450206741@qq.com>
2023-07-20 11:12:46 +0800

    Update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import eslint from '@nabla/vite-plugin-eslint'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import { fileURLToPath, URL } from 'node:url'
+
+function joinPath(path: string) {
+  return fileURLToPath(new URL(path, import.meta.url))
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslint({
+      eslintOptions: {
+        cacheLocation: joinPath('node_modules/.eslintcache'),
+      },
+    }),
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': joinPath('src'),
+    },
+  },
 })
