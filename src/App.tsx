e7a8539f2a62f59b191c1dc0b58c53a34cbb454a
Gu-Miao <1450206741@qq.com>
2025-02-24 17:59:52 +0800

    Add simple animation

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,27 @@ function App() {
     return () => masonry.destroy && masonry.destroy()
   }, [])
 
+  useEffect(() => {
+    const $ul = document.querySelector('.App ul') as HTMLUListElement
+    const $li = $ul.querySelectorAll('li') as NodeListOf<HTMLLIElement>
+    $li.forEach(($el, i) =>
+      $el.style.setProperty('--animation-delay', `${Math.min(i * 0.1, 2.25)}s`),
+    )
+    $ul.style.display = ''
+
+    return () => {}
+  }, [])
+
   return (
     <div className="App">
       <header>
         <h1>Useful</h1>
         <p>Useful websites of image resource, tools, free api, blogs etc...</p>
+        <p>
+          Use <b>Ctrl + F</b> to search.
+        </p>
       </header>
-      <ul>
+      <ul style={{ display: 'none' }}>
         {websites.map(site => (
           <li key={site.address} className="item">
             <Card {...site} />
