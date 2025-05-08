40bb3a352ceb73fd983b22cc138f88c335661ef7
Gu-Miao <1450206741@qq.com>
2025-05-08 11:55:59 +0800

    Update websites

--- src/websites.ts
+++ src/websites.ts
@@ -894,6 +894,19 @@ const websites: Website[] = [
     address: 'https://yiyan.baidu.com/',
     keywords: ['AI'],
   },
+  {
+    title: '秘塔AI搜索',
+    description: '秘塔AI搜索，没有广告，直达结果',
+    address: 'https://metaso.cn/',
+    keywords: ['AI'],
+  },
+  {
+    title: 'DeepSeek - 深度求索',
+    description:
+      '深度求索（DeepSeek），成立于2023年，专注于研究世界领先的通用人工智能底层模型与技术，挑战人工智能前沿性难题。基于自研训练框架、自建智算集群和万卡算力等资源，深度求索团队仅用半年时间便已发布并开源多个百亿级参数大模型，如DeepSeek-LLM通用大语言模型、DeepSeek-Coder代码大模型，并在2024年1月率先开源国内首个MoE大模型（DeepSeek-MoE），各大模型在公开评测榜单及真实样本外的泛化效果均有超越同级别模型的出色表现。和 DeepSeek AI 对话，轻松接入 API。',
+    address: 'https://www.deepseek.com/',
+    keywords: ['AI'],
+  },
 
   // 翻译
   {
